42f6abf40c8364adb811f63c0e872ff0c917f83f
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-04-28 22:22:21 +0200

    Adding Screenshot

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Reactive Vue Shopping Cart</h1>
+        <p class="lead">This shopping cart is made in VUE</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div class="col-md-6" v-bind:key="product.id" v-for="product in products">
+            <product
+              v-on:add-to-cart="addToCart(product)"
+              :product="product"
+              :isInCart="isInCart(product)"
+            ></product>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5 my-5">
+        <cart v-on:remove-from-cart="removeFromCart($event)" v-on:pay="pay()" :items="cart"></cart>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import products from "@/products.json";
+import Cart from "@/components/Cart";
+import Product from "@/components/Product";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Product,
+    Cart
+  },
+  methods: {
+    addToCart(product) {
+      this.cart.push(product);
+    },
+    isInCart(product) {
+      const item = this.cart.find(p => p.id === product.id);
+      return !!item;
+    },
+    removeFromCart(product) {
+      this.cart = this.cart.filter(item => item.id !== product.id);
+    },
+    pay() {
+      this.cart = [];
+      alert("Your shopping is finished!!");
+    }
+  },
+  data() {
+    return { products, cart: [] };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #fbf8f3;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="card my-5">
+    <div class="card-body">
+      <h5 class="text-center card-title">{{product.title}}</h5>
+      <p class="text-center text-muted card-text display-4">${{ Number(product.price)}}</p>
+      <button
+        @click="$emit('add-to-cart', product)"
+        :disabled="isInCart"
+        class="btn btn-primary form-control"
+      >{{ isInCart ? "Added to cart" : "Add to cart"}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["product", "isInCart"]
+};
+</script>
